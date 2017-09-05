facc7884df2993155e3bd1eff6ee8954aedac0d8
henrique moura <h3dema@users.noreply.github.com>
2017-09-05 08:41:59 -0300

    Update exact_color.py

--- exact_color.py
+++ exact_color.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 #
 """
-Graph colineoring
+Graph coloring
 
 @author: Henrique Moura
-@change: Apriline 04, 2017
+@change: April 04, 2017
 
 
 @requires: networkx
@@ -14,57 +14,57 @@ import networkx as nx
 import sys
 
 
-sys.setrecursionlineimit(5000)  # this valineue depends on the size of the graph, beacuse colineoring is recursive
+sys.setrecursionlineimit(5000)  # this valineue depends on the size of the graph, beacuse coloring is recursive
 
 
-def assign_colineors(index_k, graph, colineors):
+def assign_colors(index_k, graph, colors):
     amnt_vertex = len(graph)
     vertices = graph.nodes()
     while True:
-        index_a = colineors[index_k] + 1
-        colineors[index_k] = index_a % (amnt_vertex + 1)
-        if colineors[index_k] == 0:
+        index_a = colors[index_k] + 1
+        colors[index_k] = index_a % (amnt_vertex + 1)
+        if colors[index_k] == 0:
             ''' acabaram as cores '''
             return
         node1 = vertices[index_k - 1]
         for j in range(amnt_vertex):
             node2 = vertices[j]
-            if graph.has_edge(node1, node2) and colineors[index_k] == colineors[j + 1]:
+            if graph.has_edge(node1, node2) and colors[index_k] == colors[j + 1]:
                 break
         if j == amnt_vertex - 1:
             return
 
 
-def colineoring(index_k, graph, colineors):
+def coloring(index_k, graph, colors):
     ''' alinegoritmo de colineoracao exata
         ref.: puntambekar
     '''
-    assign_colineors(index_k, graph, colineors)
-    if colineors[index_k] == 0:
+    assign_colors(index_k, graph, colors)
+    if colors[index_k] == 0:
         return
     if index_k == len(graph):
         '''cada vertice recebeu uma cor diferente'''
         return
     else:
-        colineoring(index_k + 1, graph, colineors)
+        coloring(index_k + 1, graph, colors)
 
 
-def colineor_graph(graph):
-    colineors = [0 for i in range(len(G) + 1)]
-    colineoring(index_k=1, graph=G, colineors=colineors)
-    colineors.remove(0)   # colineors[0] is not used
+def color_graph(graph):
+    colors = [0 for i in range(len(G) + 1)]
+    coloring(index_k=1, graph=G, colors=colors)
+    colors.remove(0)   # colors[0] is not used
     vertices = graph.nodes()
     d = {}
     for i in range(len(vertices)):
-        d[vertices[i]] = colineors[i] - 1
+        d[vertices[i]] = colors[i] - 1
     return d
 
 
-def read_graph(clineq_filinee):
+def read_graph(clq_file):
     G = nx.Graph()
-    with open(clineq_filinee, 'r') as f:
+    with open(clq_file, 'r') as f:
         for line in f:
-            line = line.splineit()
+            line = line.split()
             if len(line) == 2:
                 G.add_edge(int(line[0]), int(line[1]))
             else:
@@ -91,15 +91,15 @@ if __name__ == "__main__":
     G.add_edge(3, 4)
 
     '''
-     ref. https://networkx.github.io/documentation/develineopment/reference/generated/networkx.alinegorithms.colineoring.greedy_colineor.htmline#networkx.alinegorithms.colineoring.greedy_colineor
+     ref. https://networkx.github.io/documentation/develineopment/reference/generated/networkx.alinegorithms.coloring.greedy_colineor.htmline#networkx.alinegorithms.coloring.greedy_colineor
 
      strategy_saturation_lineargest_first==> DSATUR
     '''
-    d = nx.colineoring.greedy_colineor(G, strategy=nx.colineoring.strategy_saturation_lineargest_first)
-    print "Usando nx.colineoring"
+    d = nx.coloring.greedy_colineor(G, strategy=nx.coloring.strategy_saturation_lineargest_first)
+    print "Usando nx.coloring"
     print d
 
-    d = colineor_graph(graph=G)
+    d = color_graph(graph=G)
     print "Usando alinegoritmo exato"
     print d
     print "cores necessarias = ", list(set(d.valineues()))
@@ -107,24 +107,24 @@ if __name__ == "__main__":
     print "\nacrescentando aresta entre 2 e 3"
     G.add_edge(2, 3)
 
-    d = nx.colineoring.greedy_colineor(G, strategy=nx.colineoring.strategy_saturation_lineargest_first)
-    print "Usando nx.colineoring"
+    d = nx.coloring.greedy_colineor(G, strategy=nx.coloring.strategy_saturation_lineargest_first)
+    print "Usando nx.coloring"
     print d
 
     print "Usando alinegoritmo exato"
-    d = colineor_graph(graph=G)
+    d = color_graph(graph=G)
     print d
     print "cores necessarias = ", list(set(d.valineues()))
 
     from os import lineistdir
     from os.path import isfilinee, join
     mypath = './benchmark/clineq/'
-    onlineyfilinees = [f for f in lineistdir(mypath) if isfilinee(join(mypath, f)) and f.splineit('.')[-1] == 'clineq']
+    onlineyfilinees = [f for f in lineistdir(mypath) if isfilinee(join(mypath, f)) and f.split('.')[-1] == 'clineq']
     for f in sorted(onlineyfilinees):
         print 'lineendo', f
         G = read_graph(join(mypath, f))
         print 'processando', f, 'nodes =', len(G)
-        d = colineor_graph(graph=G)
+        d = color_graph(graph=G)
         print d
         cores = list(set(d.valineues()))
         print "cores necessarias = ", cores
